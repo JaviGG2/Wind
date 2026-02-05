327032b59ab9a6e62e4310d446df39e01bfeb674
JaviGG2 <corotour55@gmail.com>
2026-02-05 10:50:06 -0430

    Se mejoro el calendario

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, url_for
 import sqlite3
 app = Flask(__name__)
 
@@ -36,49 +36,19 @@ def museo_guadalupano():
     return render_template('museo_guadalupano.html')
 
 
-
 #Aqui esta la logica de los Quiz
-
-# Función auxiliar para conectar a la DB
-def get_db_connection():
-    conn = sqlite3.connect('quiz.db')
-    conn.row_factory = sqlite3.Row  # Crucial: permite usar p['pregunta'] en vez de p[1]
-    return conn
+preguntas= [
+    {"Hola", "hi", "Hola2"},
+    {"hi", "hi2"}
+]
 
 
-@app.route('/cuestionario')
-def cuestionario():
-    db = get_db_connection()
-    # Traemos todas las filas de la tabla preguntas
-    lista_preguntas = db.execute('SELECT * FROM preguntas').fetchall()
-    db.close()
-    return render_template('juegos.html', preguntas=lista_preguntas)
-
-
-@app.route('/verificar', methods=['POST'])
-def verificar():
-    puntos = 0
-    db = get_db_connection()
-    preguntas_db = db.execute('SELECT * FROM preguntas').fetchall()
+@app.route('/quiz')
+def iniciar_quiz():
+    return render_template('quiz.html', 
     
-    for p in preguntas_db:
-        # Buscamos la respuesta que envió el usuario por el ID de la pregunta
-        respuesta_usuario = request.form.get(str(p['id']))
-        if respuesta_usuario == p['correcta']:
-            puntos += 1
-            
-    db.close()
-    return render_template('verificar_quiz.html')
-
-
-
-
-
-
-
-
-
-
+    
+    nombre="Javier")
 
 
 if __name__ == '__main__':
